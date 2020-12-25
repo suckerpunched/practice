547c3bb48b87238677abf5f3f847c88e014df08b
suckerpunched <suckerpunched@selfquery.dev>
2020-12-24 23:24:55 -0800

    day-1

--- advent-of-code/day-1-report-repair.py
+++ advent-of-code/day-1-report-repair.py
@@ -1,5 +1,3 @@
 from itertools import combinations
-
-input_data = [int(x) for x in open('day-1-report-repair.input').read().split('\n')]
-
+input_data = [int(x) for x in open('input').read().split('\n')]
 print([ a*b for a,b in combinations(input_data, 2) if a+b == 2020 ][0])
