0dfe56cceff75930f823895d3940623d6797bba1
suckerpunched <suckerpunched@selfquery.dev>
2020-12-25 17:14:33 -0800

    part-2

--- advent-of-code/day-2-password-philosophy.py
+++ advent-of-code/day-2-password-philosophy.py
@@ -1,6 +1,9 @@
 def is_valid(line):
     r,t,s = line.split(' ')
     r = r.split('-')
-    return int(r[0]) <= s.count(t.strip(':')) <= int(r[1])
+    return [
+        s[int(r[0])-1] == t.strip(':'), 
+        s[int(r[1])-1] == t.strip(':'),
+    ].count(True) == 1
 
 print(sum([ is_valid(x) for x in open('input').read().split('\n') ]))
